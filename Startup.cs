25d9d8340139be6c878e88ef51e2df5d5366d857
Abhishikha Maurya <abhishikha.maurya1998@gmail.com>
2021-10-07 22:54:00 +0530

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace testProject
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger <Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseRouting();
+            //
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapGet("/", async context =>
+            //     {
+            //         await context.Response.WriteAsync("Hello World!");
+            //     });
+            // });
+
+            app.Use(async (context, next) =>
+            {
+                // await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                // await context.Response.WriteAsync(_config["MyKey"]);
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing Response");
+            });
+
+            app.UseStaticFiles(); // can access via URL: https://localhost:5201/Images/Abhishikha.jpeg
+            app.Use(async (context, next) =>
+            {
+                // await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                // await context.Response.WriteAsync(_config["MyKey"]);
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing Response");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                // await context.Response.WriteAsync(_config["MyKey"]);
+                logger.LogInformation("MW3: Request handled and response produced");
+            });
+        }
+    }
+}
